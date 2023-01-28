1a40e3115e367a4a9905c7827b647b0eabb617f6
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2023-01-28 09:15:22 -0400

    Text Area input

--- src/lib/TextInputs.svelte
+++ src/lib/TextInputs.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+  // Text area-related import
+  import { marked } from "marked";
+
   let name = "Naruto";
 
   let a = 1;
   let b = 2;
 
   let active = false;
+
+  // Group inputs
+  let scoops = 1;
+  let flavours = ["Mint choc chip"];
+  let menu = ["Cookies and cream", "Mint choc chip", "Raspberry ripple"];
+
+  const join = (flavours: string[]) => {
+    if (flavours.length === 1) return flavours[0];
+
+    return `${flavours.slice(0, -1).join(", ")} and ${
+      flavours[flavours.length - 1]
+    }`;
+  };
+
+  // Text area
+  let textAreaValue = `Some words are *italic*, some are **bold**`;
 </script>
 
 <h1>Text Inputs</h1>
@@ -28,6 +47,8 @@
 
 <p>{a} + {b} = {a + b}</p>
 
+<h2>Checkbox</h2>
+
 <label>
   <input type="checkbox" bind:checked={active} />
   Yes! Send me regular email spam
@@ -40,3 +61,60 @@
 {/if}
 
 <button disabled={!active}> Subscribe </button>
+
+<h2>Size</h2>
+
+<label>
+  <input type="radio" bind:group={scoops} name="scoops" value={1} />
+  One scoop
+</label>
+
+<label>
+  <input type="radio" bind:group={scoops} name="scoops" value={2} />
+  Two scoops
+</label>
+
+<label>
+  <input type="radio" bind:group={scoops} name="scoops" value={3} />
+  Three scoops
+</label>
+
+<h2>Flavours</h2>
+
+{#each menu as flavour}
+  <label>
+    <input
+      type="checkbox"
+      bind:group={flavours}
+      name="flavours"
+      value={flavour}
+    />
+    {flavour}
+  </label>
+{/each}
+
+{#if !flavours.length}
+  <p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+  <p>Can't order more flavours than scoops!</p>
+{:else}
+  <p>
+    You ordered {scoops}
+    {scoops === 1 ? "scoop" : "scoops"}
+    of {join(flavours)}
+  </p>
+{/if}
+
+<h2>Textarea Inputs</h2>
+
+{@html marked(textAreaValue)}
+
+<textarea bind:value={textAreaValue} />
+
+<style>
+  textarea {
+    width: 100%;
+    height: 100px;
+    margin-bottom: 2em;
+  }
+</style>
